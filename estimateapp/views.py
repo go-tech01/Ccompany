0ee89c20f52cfbc9d29585f5c8f9d69de15f0ff3
go-tech01 <zoneofgod@gmail.com>
2021-11-11 16:20:50 +0900

    11/11 1

--- estimateapp/views.py
+++ estimateapp/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,20 +7,30 @@ from django.views.generic import CreateView, DetailView, TemplateView
 
 from estimateapp.forms import ImageCreationForm
 from estimateapp.models import EstimateModel, Output
+from estimateapp.process import Process
 
 
 class CreateEstimateView(CreateView):
     model = EstimateModel
     form_class = ImageCreationForm
     template_name = 'estimateapp/create.html'
+    # def gitrequestfiles(self, request):
+    #     print(request.files)
+    #     data = {
+    #         "name":request.files
+    #     }
+    #     return JsonResponse(data)
     def get_success_url(self):
-        # print(self.object.id)
         obj = EstimateModel.objects.get(pk=self.object.pk)
+        print(obj)
         out_11 = Output()
-        out_11.list_11 = obj.df()
-        out_11.list_22 = obj.construction()
-        out_11.list_33 = obj.detail()
+        input = str(obj.input_estimateimage)
+        process_1 = Process(input)
+        out_11.list_11 = process_1.df()
+        out_11.list_22 = process_1.construction()
+        out_11.list_33 = process_1.detail()
         out_11.save()
+        # self.gitrequestfiles(self, request)
         return reverse('estimateapp:detail', kwargs={'pk': self.object.pk})
 
 class OutputImageView(DetailView):
