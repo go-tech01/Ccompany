c02fc86d99e29b8f8fde3fcb4b65223808ba879a
go-tech01 <zoneofgod@gmail.com>
2021-11-22 14:28:19 +0900

    11/22 SECRET_KEY

--- Ccompany/settings.py
+++ Ccompany/settings.py
@@ -16,11 +16,25 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+env_list = dict()
+
+local_env = open(os.path.join(BASE_DIR, '.env'), encoding='utf-8')
+
+while True:
+    line = local_env.readline()
+    if not line:
+        break
+    line = line.replace('\n', '')
+    start = line.find('=')
+    key = line[:start]
+    value = line[start+1:]
+    env_list[key] = value
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e^bjt0z)t$f%viq&a*dk9wdiddk4obubjese=5j_spjs8d(u#6'
+SECRET_KEY = env_list['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
